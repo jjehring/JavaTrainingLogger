47a8a0a72ede90a5abf1cf15c61af3c6300ec5fa
jjehring <joerg.jehring@vodafone.com>
2020-03-06 13:39:12 +0100

    in

---
+++ src/main/java/myLogger.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+
+public class myLogger {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("myLog");
+        logger.error("ERROR");
+        logger.debug("DEBUG");
+        logger.info("Just an Info");
+
+    }
+}
